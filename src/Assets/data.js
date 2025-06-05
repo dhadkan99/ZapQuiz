be98166902f235a8125e00169429107e3f558f75
Dhadkan <dhadkankc88@gmail.com>
2025-06-05 21:17:06 +0545

    added few more questions

--- src/Assets/data.js
+++ src/Assets/data.js
@@ -15,5 +15,30 @@ export const data = [
       option4: "Africa",
       ans: 4,
     },
+    {
+        question: "What does CPU stand for?",
+        option1: "Central Processing Unit",
+        option2: "Computer Personal Unit",
+        option3: "Central Performance Utility",
+        option4: "Core Programming Unit",
+        ans: 1,
+    },
+    {
+        question: "Which language is used to style web pages?",
+        option1: "HTML",
+        option2: "Python",
+        option3: "CSS",
+        option4: "C++",
+        ans: 3,
+    },
+    {
+        question: "What is the boiling point of water at sea level?",
+        option1: "90°C",
+        option2: "100°C",
+        option3: "110°C",
+        option4: "120°C",
+        ans: 2,
+    },
+      
   ];
   
